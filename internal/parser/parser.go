828ac669483c5a83a942debc958857ec3607e65b
FranChesKo <dev.franchesko@gmail.com>
2024-06-26 01:04:43 +0800

    implement FetchPuzzle function

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,6 +1,10 @@
 package parser
 
-import "github.com/yuin/gopher-lua"
+import (
+	"fmt"
+
+	"github.com/yuin/gopher-lua"
+)
 
 // TODO: move const and type to constants
 type (
@@ -32,7 +36,39 @@ type Puzzle struct {
 	Layout      []TileType
 }
 
-func FetchPuzzle(fileName string) (*Puzzle, error)
+// TODO: use goroutines to call all fetch functions
+// TODO: refactor FetchPuzzle function
+func FetchPuzzle(fileName string) (*Puzzle, error) {
+	L := lua.NewState()
+	defer L.Close()
+	if err := L.DoFile(fileName); err != nil {
+		return &Puzzle{}, fmt.Errorf("unable to load lua script %s: %w", fileName, err)
+	}
+
+	title, err := fetchTitle(L)
+	if err != nil {
+		return &Puzzle{}, err
+	}
+	description, err := fetchDescription(L)
+	if err != nil {
+		return &Puzzle{}, err
+	}
+	streams, err := fetchStreams(L)
+	if err != nil {
+		return &Puzzle{}, err
+	}
+	layout, err := fetchLayout(L)
+	if err != nil {
+		return &Puzzle{}, err
+	}
+
+	return &Puzzle{
+		Title:       title,
+		Description: description,
+		Streams:     streams,
+		Layout:      layout,
+	}, nil
+}
 
 func runLuaFunction(L *lua.LState, functionName string) (lua.LValue, error)
 

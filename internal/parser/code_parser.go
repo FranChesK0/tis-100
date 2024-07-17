920a153193fab4a39a837bed3caa5e603abeb1ae
FranChesKo <dev.franchesko@gmail.com>
2024-07-17 16:28:09 +0800

    return file path after saving

--- internal/parser/code_parser.go
+++ internal/parser/code_parser.go
@@ -11,35 +11,35 @@ import (
 	"github.com/FranChesK0/tis-100/internal/types"
 )
 
-func SaveCode(dirPath string, code *types.ProgramCode) error {
+func SaveCode(dirPath string, code *types.ProgramCode) (string, error) {
 	_, err := os.ReadDir(dirPath)
 	if err != nil {
-		return fmt.Errorf("unable to read directory %s: %w", dirPath, err)
+		return "", fmt.Errorf("unable to read directory %s: %w", dirPath, err)
 	}
 
 	filePath := filepath.Join(dirPath, fmt.Sprintf("%s.tis", strings.ToLower(code.Title)))
 
 	file, err := os.Create(filePath)
 	if err != nil {
-		return fmt.Errorf("unable to create file with name %s: %w", filePath, err)
+		return "", fmt.Errorf("unable to create file with name %s: %w", filePath, err)
 	}
 	defer file.Close()
 
 	for i, node := range code.NodesCode {
 		if _, err = file.WriteString(fmt.Sprintf("@%d\n", i+1)); err != nil {
-			return fmt.Errorf("error while writing data to file %s: %w", filePath, err)
+			return "", fmt.Errorf("error while writing data to file %s: %w", filePath, err)
 		}
 		for _, str := range node {
 			if _, err = file.WriteString(str + "\n"); err != nil {
-				return fmt.Errorf("error while writing data to file %s: %w", filePath, err)
+				return "", fmt.Errorf("error while writing data to file %s: %w", filePath, err)
 			}
 		}
 		if _, err = file.WriteString("\n"); err != nil {
-			return fmt.Errorf("error while writing data to file %s: %w", filePath, err)
+			return "", fmt.Errorf("error while writing data to file %s: %w", filePath, err)
 		}
 	}
 
-	return nil
+	return file.Name(), nil
 }
 
 func FetchCode(fileName string) (*types.ProgramCode, error) {

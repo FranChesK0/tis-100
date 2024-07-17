e7bbd7751fab56fb97424c3e89e6108376a4eb2b
FranChesKo <dev.franchesko@gmail.com>
2024-07-18 01:49:40 +0800

    implement append method for NodeList

--- internal/emu/node_list.go
+++ internal/emu/node_list.go
@@ -5,4 +5,19 @@ type NodeList struct {
 	Next *NodeList
 }
 
-func Append(list *NodeList, n *Node) *NodeList
+func Append(list *NodeList, n *Node) *NodeList {
+	tail := &NodeList{
+		Node: n,
+		Next: nil,
+	}
+	if list == nil {
+		return tail
+	}
+
+	head := list
+	for head.Next != nil {
+		head = head.Next
+	}
+	head.Next = tail
+	return list
+}

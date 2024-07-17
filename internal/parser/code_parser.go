74572ab64aa2da16756ddd2e3fe0242d7699cbf6
FranChesKo <dev.franchesko@gmail.com>
2024-07-17 14:38:39 +0800

    add fetch code function

--- internal/parser/code_parser.go
+++ internal/parser/code_parser.go
@@ -1,10 +1,13 @@
 package parser
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/FranChesK0/tis-100/internal/constants"
 	"github.com/FranChesK0/tis-100/internal/types"
 )
 
@@ -37,4 +40,43 @@ func SaveCode(dirPath string, code *types.ProgramCode) error {
 	}
 
 	return nil
+}
+
+func FetchCode(fileName string) (*types.ProgramCode, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open file %s: %w", fileName, err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	nodesCode := make([][]string, constants.NodesNumber)
+
+	var line string
+	curNode := -1
+	for scanner.Scan() {
+		line = strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		if strings.HasPrefix(line, "@") {
+			curNode++
+			continue
+		}
+
+		nodesCode[curNode] = append(nodesCode[curNode], line)
+	}
+
+	if err = scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error while reading file %s: %w", fileName, err)
+	}
+
+	title := filepath.Base(file.Name())
+	title = strings.ToUpper(strings.TrimSuffix(title, ".tis"))
+
+	return &types.ProgramCode{
+		Title:     title,
+		NodesCode: nodesCode,
+	}, nil
 }

98d483388dea9751cc2676034b59b0e3c6ec35e1
FranChesKo <dev.franchesko@gmail.com>
2024-07-19 20:26:05 +0800

    add key map

---
+++ internal/tui/keys.go
@@ -0,0 +1,56 @@
+package tui
+
+import "github.com/charmbracelet/bubbles/key"
+
+type keyMap struct {
+	Open key.Binding
+	Save key.Binding
+	Next key.Binding
+	Prev key.Binding
+	Run  key.Binding
+	Help key.Binding
+	Quit key.Binding
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.Quit}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Open, k.Save},
+		{k.Next, k.Prev, k.Run},
+		{k.Help, k.Quit},
+	}
+}
+
+var keys = keyMap{
+	Open: key.NewBinding(
+		key.WithKeys("ctrl+o"),
+		key.WithHelp("ctrl+o", "open puzzle"),
+	),
+	Save: key.NewBinding(
+		key.WithKeys("ctrl+s"),
+		key.WithHelp("ctrl+s", "save your solution"),
+	),
+	Next: key.NewBinding(
+		key.WithKeys("tab"),
+		key.WithHelp("tab", "move to next mode"),
+	),
+	Prev: key.NewBinding(
+		key.WithKeys("shift+tab"),
+		key.WithHelp("shift+tab", "move to previous node"),
+	),
+	Run: key.NewBinding(
+		key.WithKeys("ctrl+r"),
+		key.WithHelp("ctrl+r", "run program"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c"),
+		key.WithHelp("ctrl+c", "quit"),
+	),
+}
